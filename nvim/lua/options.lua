ebd64587a296453d00f5f7d9b0ea3c380db90c06
Mateo Cindrić <mateocindricdev@gmail.com>
2024-11-20 16:14:02 +0100

    Initial setup

---
+++ nvim/lua/options.lua
@@ -0,0 +1,13 @@
+require "nvchad.options"
+
+-- add yours here!
+
+local o = vim.o
+o.cursorlineopt = "both" -- to enable cursorline!
+-- add yours here!
+o.softtabstop = 2
+o.tabstop = 2
+o.shiftwidth = 2
+o.expandtab = true
+o.textwidth = 80
+o.relativenumber = true
